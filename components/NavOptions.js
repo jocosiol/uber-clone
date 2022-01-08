e40c5e4bbbab4c9bce7606bc620b9abb7117ee23
jocosiol <jocosiol@gmail.com>
2022-01-08 21:37:56 +0200

    init build

---
+++ components/NavOptions.js
@@ -0,0 +1,55 @@
+import { useNavigation } from "@react-navigation/native";
+import React from "react";
+import { Text, FlatList, TouchableOpacity, View, Image } from "react-native";
+import { Icon } from "react-native-elements";
+import tw from "tailwind-react-native-classnames";
+
+const data = [
+  {
+    id: "123",
+    title: "Get a ride",
+    image:
+      "https://www.uber-assets.com/image/upload/f_auto,q_auto:eco,c_fill,w_956,h_637/v1555367310/assets/30/51e602-10bb-4e65-b122-e394d80a9c47/original/Final_UberX.png",
+    screen: "MapScreen",
+  },
+  {
+    id: "456",
+    title: "Order food",
+    image:
+      "https://res.cloudinary.com/imgpetapp/image/upload/v1641500337/food_bag_alqear.png",
+    screen: "EatsScreen", //Change in future...
+  },
+];
+
+const NavOptions = () => {
+  const navigation = useNavigation();
+  return (
+    <FlatList
+      data={data}
+      horizontal
+      keyExtractor={(item) => item.id}
+      renderItem={({ item }) => (
+        <TouchableOpacity
+          onPress={() => navigation.navigate(item.screen)}
+          style={tw`p-2 pl-6 pb-8 pt-4 bg-gray-200 m-2 w-40`}
+        >
+          <View>
+            <Image
+              style={{ width: 120, height: 120, resizeMode: "contain" }}
+              source={{ uri: item.image }}
+            />
+            <Text style={tw`mt-2 text-lg font-semibold`}>{item.title}</Text>
+            <Icon
+              style={tw`p-2 bg-black rounded-full w-10 mt-4`}
+              name="arrowright"
+              color="white"
+              type="antdesign"
+            />
+          </View>
+        </TouchableOpacity>
+      )}
+    />
+  );
+};
+
+export default NavOptions;
